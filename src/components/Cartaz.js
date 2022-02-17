7fb1c87cd9073aad3171801eea9341002f1e4dcc
Henrique Saenger <henriquesaenger@gmail.com>
2022-02-17 00:17:08 -0300

    Cartaz responsivo

--- src/components/Cartaz.js
+++ src/components/Cartaz.js
@@ -4,16 +4,25 @@ import "./Cartaz.css";
 
 const Cartaz = () => {
 
-    return(
-        <div className="cartaz">
-            <div className="cartaz_foto">
+    return (
+        <div className="cartaz-wrapper">
+            <div className="cartaz">
+                <div className="cartaz_foto">
+
+                </div>
+                <div className="cartaz_info">
+                    <h1>Seja bem-vindo à plataforma de estudo Appanese</h1>
+                    <p>Uma plataforma desenvolvida por um estudante com a missão de ajudar outros estudantes a suavizar sua caminhada rumo às provas de proficiência em japonês(JLPT- Japanese Language Proficiency Test), de uma forma descontraída.</p>
+                    <button>Cadastre-se</button>
+                </div>
             </div>
-            <div className="cartaz_info">
+            <div className="cartaz_info_menor">
                 <h1>Seja bem-vindo à plataforma de estudo Appanese</h1>
                 <p>Uma plataforma desenvolvida por um estudante com a missão de ajudar outros estudantes a suavizar sua caminhada rumo às provas de proficiência em japonês(JLPT- Japanese Language Proficiency Test), de uma forma descontraída.</p>
                 <button>Cadastre-se</button>
             </div>
         </div>
+
     )
 
 
